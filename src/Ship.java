0141eed3316ec025798e82cb17105fc7340612a0
tannermcgowan <tjmcgowan1@gmail.com>
2015-05-18 21:03:41 -0400

    Some Idea Changes To Ship
    In this I just used a lot of comments for ideas, because we aren’t
    completely sure how we are going to do stuff yet

--- src/Ship.java
+++ src/Ship.java
@@ -6,5 +6,36 @@ public class Ship {
 	 * and some others
 	 * 
 	 */
-
+	private boolean isSunk;
+	private int length;
+	//Some attribute to show which way they are facing. I.E. North, West, etc.
+	
+	
+	
+	public Ship(int l){
+		//this needs to be done as buttons once we have the layout figured out, but these are some attributes needed
+		
+		isSunk = false;
+		length = l;
+		//something about direction, maybe something to do with user interput?
+	}
+	
+	public void getSunk(){
+		//this maybe can have something to do with the JButtons becoming all red, if we end up using buttons
+	}
+	
+	public void takeHit(){
+		//this method should turn the JButton, or whatever area depending on how we do it, turns red
+	}
+	
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+																								//
+																								//
+																								//
+					//The ship placing set of methods could possibly go here																			//
+																								//
+																								//
+																								//
+																								//
+	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 }
