32be7de882fabfb0b7b87fcc6647c3e58d253bbf
hansf16 <hfotta@exeter.edu>
2015-05-19 19:17:41 -0400

    Just a gitHub error or something

--- src/Ship.java
+++ src/Ship.java
@@ -8,16 +8,15 @@ public class Ship {
 	 */
 	private boolean isSunk;
 	private int length;
-	//Some attribute to show which way they are facing. I.E. North, West, etc.
+	private boolean isVertical;
 	
 	
 	
 	public Ship(int l){
-		//this needs to be done as buttons once we have the layout figured out, but these are some attributes needed
 		
 		isSunk = false;
 		length = l;
-		//something about direction, maybe something to do with user interput?
+		isVertical = true;
 	}
 	
 	public void getSunk(){
@@ -28,14 +27,4 @@ public class Ship {
 		//this method should turn the JButton, or whatever area depending on how we do it, turns red
 	}
 	
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-																								//
-																								//
-																								//
-					//The ship placing set of methods could possibly go here																			//
-																								//
-																								//
-																								//
-																								//
-	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 }
