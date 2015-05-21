bd2f56fdd89ba5efc985b19661e55a3a8facb7d3
tannermcgowan <tjmcgowan1@gmail.com>
2015-05-21 12:18:19 -0400

    Update to data class in class 5-21
    *NOTICE:
    in the CPUFireMissile and the playerFireMissile methods, they return a
    string of 2 numbers that will be the coordinates where the shot was
    fired. If the game is over and this method still runs, it will return
    “a”. Make sure to code for this in case this happens.

--- src/Data.java
+++ src/Data.java
@@ -18,9 +18,18 @@ public class Data {
 	int [][] playerGrid = new int [10][10];
 	
 	
+	public Data(){
+			
+	}
+	
+	
+	
+	
+	
 	public void updateStats(){
+		if(checkForWin()==false){
 		//this will update the stats
-		
+
 		accuracy = numberOfShotsFired / numberOfShotsThatHit;
 		 
 		accuracyForTheScoreboard = "Your Accuracy Is " + accuracy*100 + "%";
@@ -32,7 +41,7 @@ public class Data {
 		numberOfCPUSHipsLeftForTheScoreboard = "The enemy has " + numberOfCPUSHipsLeft +  " ships left";
 		
 		numberOfPlayerShipsLeftForTheScoreboard = "The enemy has sunken " + (5 - numberOfPlayerShipsLeft) + " of your ships";
-		
+		}
 	}
 	
 	
@@ -47,42 +56,51 @@ public class Data {
 	
 	
 	public String playerFireMissile(){
-		//this is the method where the player fires a missile. Should show up
+		if(checkForWin()==false){//this is the method where the player fires a missile. Should show up
 		numberOfShotsFired++;
 		updateStats();
 		//we decided we would return a string of 2 numbers, which we can then split and use as coordinates
 		return "00";
+		}
+		return"a";
 	}
 	
 	
 	public String CPUFireMissile(){
-		//This will be the method where the CPU fires a missile.. If it hits it should fire at the surrounding 8 spaces
+		if(checkForWin()==false){//This will be the method where the CPU fires a missile.. If it hits it should fire at the surrounding 8 spaces
 		updateStats();
 		//we decided we would return a string of 2 numbers, which we can then split and use as coordinates
 		return "00";
+		}
+		return"a";
 	}
 	
 	public void CPUTakeHit(){
-		//this will be the method that scores the data when the CPU bopat takes a hit
+		if(checkForWin()==false){//this will be the method that scores the data when the CPU bopat takes a hit
 		numberOfShotsThatHit++;
 		updateStats();
+		}
 	}
 	
 	public void CPUGetSunk(){
-		//this is the method where the CPU gets a boat sunken
+		if(checkForWin()==false){//this is the method where the CPU gets a boat sunken
 		numberOfCPUSHipsLeft--;
 		updateStats();
+		}
 	}
 	
 	public void playerGetSunk(){
+		if(checkForWin()==false){
 		//this is the method where the player loses a ship, and the buttons all turn red
 		numberOfPlayerShipsLeft--;
 		updateStats();
+		}
 	}
 	
 	public void playerTakeHit(){
-		//this method is where the JButton that was hit turns red, and stores the data that it was hit
+		if(checkForWin()==false){//this method is where the JButton that was hit turns red, and stores the data that it was hit
 		updateStats();
+		}
 	}
 	
 	
